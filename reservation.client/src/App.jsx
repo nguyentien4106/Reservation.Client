be73c68c9391910f00c7a16a483883ec4b9bb571
walter.nguyen <walter.nguyen@nexcel.com>
2024-05-24 11:12:19 +0700

    integrate react-ga4

--- reservation.client/src/App.jsx
+++ reservation.client/src/App.jsx
@@ -1,32 +1,43 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import './App.css';
-import Loading from './components/common/Loading';
-import { useSelector } from 'react-redux';
-import { message } from 'antd';
-import { App as AntdApp } from 'antd'
-import { AppRoutes } from './routes';
-import AppLayout from './layouts/AppLayout';
-import { UserContext } from './context/useUserContext';
-import { getUser } from './lib/helper';
-import { useState } from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Loading from "./components/common/Loading";
+import { useSelector } from "react-redux";
+import { message } from "antd";
+import { App as AntdApp } from "antd";
+import { AppRoutes } from "./routes";
+import AppLayout from "./layouts/AppLayout";
+import { UserContext } from "./context/useUserContext";
+import { getUser } from "./lib/helper";
+import { useEffect, useState } from "react";
+import ReactGA from "react-ga4";
+
+const trackingId = "G-F7XRJBGWJS"; // Replace with your Google Analytics tracking ID
+ReactGA.initialize(trackingId);
+
+ReactGA.send({
+    hitType: "pageview",
+    page: window.location.pathname,
+})
 
 function App() {
-    const isLoading = useSelector(state => state.loading.isLoading)
-    const [msg, contextHolder] = message.useMessage()
-    const [user, setUser] = useState(getUser())
+    const isLoading = useSelector((state) => state.loading.isLoading);
+    const [msg, contextHolder] = message.useMessage();
+    const [user, setUser] = useState(getUser());
+
+    useEffect(() => {
+        ReactGA.pageview(window.location.pathname + window.location.search);
+    }, []);
 
     return (
         <>
             {contextHolder}
             {isLoading && <Loading />}
             <AntdApp>
-                <UserContext.Provider value={{ user : user, setUser: setUser}}>
+                <UserContext.Provider value={{ user: user, setUser: setUser }}>
                     <BrowserRouter>
                         <Routes>
-                            <Route path='/' element={<AppLayout />} >
-                                {
-                                    AppRoutes
-                                }
+                            <Route path="/" element={<AppLayout />}>
+                                {AppRoutes}
                             </Route>
                         </Routes>
                     </BrowserRouter>
@@ -34,7 +45,6 @@ function App() {
             </AntdApp>
         </>
     );
-
 }
 
 export default App;
